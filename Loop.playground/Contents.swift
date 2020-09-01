dc03646b0926fb253acdc7dca5b2e5ad9ac0ccd9
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-01 22:28:00 +0530

    new

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// for loop
+
+for a  in 1...5{
+    print(a)
+   
+}
+for _  in 0...5{
+    print("i am gunjan")
+   
+}
+
+var sum = 0
+for a  in 1...5{
+    
+    print(sum)
+    //sum=sum+a
+    sum += a
+    
+   
+}
+//while loop
+var x = 0
+while x<5 {
+    print("i am while loop")
+    x += 1
+}
