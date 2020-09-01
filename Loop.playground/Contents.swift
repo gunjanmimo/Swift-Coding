907c9581102ce93b3febe3b8b01749e477ae583e
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-02 00:02:12 +0530

    new

--- Loop.playground/Contents.swift
+++ Loop.playground/Contents.swift
@@ -24,7 +24,15 @@ for a  in 1...5{
 }
 //while loop
 var x = 0
-while x<5 {
+while x<3 {
     print("i am while loop")
     x += 1
 }
+
+//repeat same as do while
+var counter  = 5
+repeat {
+    print("i am repeat loop\n")
+    counter = counter - 1
+} while counter>0
+
