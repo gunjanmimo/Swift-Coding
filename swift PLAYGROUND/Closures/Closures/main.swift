b55a6af107799b3b02760244bf853ff9a46b4d82
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-08 19:49:45 +0530

    new

--- swift PLAYGROUND/Closures/Closures/main.swift
+++ swift PLAYGROUND/Closures/Closures/main.swift
@@ -1,1 +1,21 @@
+func calculator(n1:Int, n2:Int,operation: (Int,Int)->Int ) -> Int{
+    return operation(n1,n2)
+}
 
+func add(n1: Int, n2:Int) -> Int {
+    return n1+n2
+}
+
+func multiply(n1: Int, n2:Int) -> Int {
+    return n1*n2
+}
+
+print(calculator(n1: 10, n2: 12,operation: add))
+
+//clouser
+print(calculator(n1: 10, n2: 20, operation: { (n1:Int,n2: Int) -> Int in
+   return n1*n2
+}))
+print(calculator(n1: 10, n2: 12,operation: { (n1, n2)  in n1*n2}))
+print(calculator(n1: 10, n2: 12,operation: {$0*$1}))
+print(calculator(n1: 10, n2: 12) {$0*$1})
