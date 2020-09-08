3f7312895479efea18050ba3dc03a6feb9190ed7
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-08 17:18:30 +0530

    new

--- swift PLAYGROUND/Protocol and Delegates/Protocol and Delegates/main.swift
+++ swift PLAYGROUND/Protocol and Delegates/Protocol and Delegates/main.swift
@@ -16,3 +16,37 @@ class EmergencyCallHandeler {
         delegate?.performCPR()
     }
 }
+
+
+
+struct Paramedic: AdvanceLifeSupport {
+    init(handler:EmergencyCallHandeler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("paramedic does chest compressions, 30 per second")
+    }
+}
+
+
+
+class Doctor: AdvanceLifeSupport {
+    init(handler: EmergencyCallHandeler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("the doctor does the chest compression, 30 per second")
+    }
+    
+    func useStatesocpe() {
+        print("listing for heart sounds")
+    }
+}
+
+
+let emilio  = EmergencyCallHandeler()
+let pete = Paramedic(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
+
