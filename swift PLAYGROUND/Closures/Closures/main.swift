2d3d3152820aa2aa95ef8a11eff0d995cf14aa2a
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-08 19:54:44 +0530

    new

--- swift PLAYGROUND/Closures/Closures/main.swift
+++ swift PLAYGROUND/Closures/Closures/main.swift
@@ -19,3 +19,13 @@ print(calculator(n1: 10, n2: 20, operation: { (n1:Int,n2: Int) -> Int in
 print(calculator(n1: 10, n2: 12,operation: { (n1, n2)  in n1*n2}))
 print(calculator(n1: 10, n2: 12,operation: {$0*$1}))
 print(calculator(n1: 10, n2: 12) {$0*$1})
+
+
+// map
+
+let arr = [4,3,34,23,2,3]
+func addTen(n1:Int)->Int{
+    return n1+10
+}
+print(arr.map({n1 in n1+100}))
+print(arr.map{$0+100})
