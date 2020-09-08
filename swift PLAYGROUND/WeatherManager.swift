57da30c326cfb07ba58157f895161731fba2a521
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-08 19:11:56 +0530

    new

---
+++ swift PLAYGROUND/WeatherManager.swift
@@ -0,0 +1,21 @@
+//
+//  Weathermanager.swift
+//  Clima
+//
+//  Created by Gunjan  Paul on 08/09/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import SwiftUI
+
+struct Weathermanager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Weathermanager_Previews: PreviewProvider {
+    static var previews: some View {
+        Weathermanager()
+    }
+}
