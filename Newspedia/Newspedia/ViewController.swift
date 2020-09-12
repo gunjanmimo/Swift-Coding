8dbc8acf7ed6a35e47a1fefc0b02da3cac1533d1
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-13 01:46:15 +0530

    new

---
+++ Newspedia/Newspedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newspedia
+//
+//  Created by Gunjan  Paul on 13/09/20.
+//  Copyright © 2020 Gunjan  Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
