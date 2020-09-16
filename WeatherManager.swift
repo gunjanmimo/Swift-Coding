e8812e880b12d5968f9d04c2bfb3a47475382ce2
Gunjan Paul <46948185+gunjanmimo@users.noreply.github.com>
2020-09-17 01:28:20 +0530

    new

--- WeatherManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WeatherManager.swift
-//  Clima
-//
-//  Created by Gunjan  Paul on 06/09/20.
-//  Copyright © 2020 App Brewery. All rights reserved.
-//
-
-import Foundation
--- WeatherManagrt.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WeatherManagrt.swift
-//  Clima
-//
-//  Created by Gunjan  Paul on 06/09/20.
-//  Copyright © 2020 App Brewery. All rights reserved.
-//
-
-import Foundation
